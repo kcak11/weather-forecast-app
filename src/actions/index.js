39f89b659caa9fcf25ac6bdec07713c911095948
K.C.Ashish Kumar <kcak11@gmail.com>
2017-01-21 11:58:15 +0530

    .
    .

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '6a78596d062df78380eff5944c4e5567';
+const API_KEY = '6bf924969a784be91dcd9199c1f109fd';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
