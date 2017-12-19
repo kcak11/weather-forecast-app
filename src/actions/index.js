061a7480c646ebe86f0bc6b8896a91bfe6f2a833
K.C.Ashish Kumar <kcak11@gmail.com>
2017-12-19 10:35:34 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '6bf924969a784be91dcd9199c1f109fd';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=${API_KEY}`;
+const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?units=imperial&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
 
   return {
