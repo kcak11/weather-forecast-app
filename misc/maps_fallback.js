e01963e5298c34678934768162aa1621d67b4a20
K.C.Ashish Kumar <kcak11@gmail.com>
2018-12-16 14:51:18 +0530

    Update maps_fallback.js

--- misc/maps_fallback.js
+++ misc/maps_fallback.js
@@ -1,8 +1,12 @@
 /*
-Google Maps fallback script to display city name instead of map during map's unavailability
-Pass parameter ?maps_fallback=no in the url to see the maps experience
+  Google Maps fallback script to display city name with map link instead of embedded map during map's unavailability
+  Pass parameter ?maps_fallback=no in the url to see the embedded maps experience
 */
-var len = document.querySelectorAll("tbody>tr").length;
+var len = document.querySelectorAll("tbody>tr").length
+  , _pinChar = decodeURIComponent("%F0%9F%93%8D");
+var loadMap = function(link) {
+    window.open("https://www.google.com/maps/place/" + link.getAttribute("data-city"), "_blank");
+};
 function handleCityDisplay(e) {
     if (e.target.className.indexOf("btn-secondary") === -1) {
         return;
@@ -25,7 +29,7 @@ function handleCityDisplay(e) {
                     attr = cell.getAttribute("data-reactid");
                     arr = attr.split(".");
                     inp = arr[arr.length - 2].split("$").join("");
-                    cell.innerHTML = "<span style=\"font-size:22px;font-weight:bold;\">" + inp + "</span>";
+                    cell.innerHTML = "<a class=\"cityDetailsLink\" href=\"#\" data-city=\"" + inp + "\" onclick=\"loadMap(this);\" style=\"font-size:22px;font-weight:bold;\">" + _pinChar + " <span class=\"cityName\">" + inp + "</span></a>";
                 } catch (exjs) {
                     window.console && console.log(exjs);
                 }
@@ -34,6 +38,14 @@ function handleCityDisplay(e) {
     };
     _chk();
 }
-if(window.location.search.indexOf("maps_fallback=no") === -1){
-  document.querySelector("body").addEventListener("click", handleCityDisplay, false);
+if (window.location.search.indexOf("maps_fallback=no") === -1) {
+    document.querySelector("body").addEventListener("click", handleCityDisplay, false);
+}
+if (window.console) {
+    var _cerr = console.error;
+    console.error = function() {
+        if (arguments[0].indexOf("request quota") === -1) {
+            _cerr.apply(console, arguments);
+        }
+    };
 }
